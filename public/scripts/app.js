5f1dbfb3f296c50923a7ae3cc5dd6fe64f3fc383
Mozetsu <mozetsu.dm@gmail.com>
2022-01-31 21:23:51 +0000

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,90 @@
 'use strict';
 
-console.log('App.js is running');
+var app = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer',
+	options: []
+};
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
+var onFormSubmit = function onFormSubmit(e) {
+	e.preventDefault();
+
+	var option = e.target.elements.option.value;
+
+	if (!option) {
+		return console.log('Option cannot be empty!');
+	}
+
+	app.options.push(option);
+	e.target.elements.option.value = '';
+	return renderApp();
+};
+
+var removeOptions = function removeOptions() {
+	app.options.length = 0;
+	return renderApp();
+};
+
+var makeDecision = function makeDecision() {
+	var randomNumber = Math.floor(Math.random() * app.options.length);
+	var option = app.options[randomNumber];
+	alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderApp = function renderApp() {
+	var template = React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'h1',
+			null,
+			app.title
+		),
+		app.subtitle && React.createElement(
+			'p',
+			null,
+			app.subtitle
+		),
+		React.createElement(
+			'p',
+			null,
+			app.options.length > 0 ? 'Here are your options' : 'No options'
+		),
+		React.createElement(
+			'button',
+			{ disabled: app.options.length === 0, onClick: makeDecision },
+			'What should I do?'
+		),
+		React.createElement(
+			'button',
+			{ disabled: app.options.length === 0, onClick: removeOptions },
+			'Remove all'
+		),
+		app.options.length > 0 && React.createElement(
+			'ol',
+			null,
+			app.options.map(function (option) {
+				return React.createElement(
+					'li',
+					{ key: app.options.length + option },
+					option
+				);
+			})
+		),
+		React.createElement(
+			'form',
+			{ onSubmit: onFormSubmit },
+			React.createElement('input', { type: 'text', name: 'option' }),
+			React.createElement(
+				'button',
+				null,
+				'Add'
+			)
+		)
+	);
+	ReactDOM.render(template, appRoot);
+};
+
+renderApp();
--- src/app.js
+++ src/app.js
@@ -1,6 +1,62 @@
-console.log('App.js is running');
+const app = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer',
+	options: [],
+};
 
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+const onFormSubmit = (e) => {
+	e.preventDefault();
 
-ReactDOM.render(template, appRoot);
+	const option = e.target.elements.option.value;
+
+	if (!option) {
+		return console.log('Option cannot be empty!');
+	}
+
+	app.options.push(option);
+	e.target.elements.option.value = '';
+	return renderApp();
+};
+
+const removeOptions = () => {
+	app.options.length = 0;
+	return renderApp();
+};
+
+const makeDecision = () => {
+	const randomNumber = Math.floor(Math.random() * app.options.length);
+	const option = app.options[randomNumber];
+	alert(option);
+};
+
+const appRoot = document.getElementById('app');
+
+const renderApp = () => {
+	const template = (
+		<div>
+			<h1>{app.title}</h1>
+			{app.subtitle && <p>{app.subtitle}</p>}
+			<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+			<button disabled={app.options.length === 0} onClick={makeDecision}>
+				What should I do?
+			</button>
+			<button disabled={app.options.length === 0} onClick={removeOptions}>
+				Remove all
+			</button>
+			{app.options.length > 0 && (
+				<ol>
+					{app.options.map((option) => {
+						return <li key={app.options.length + option}>{option}</li>;
+					})}
+				</ol>
+			)}
+			<form onSubmit={onFormSubmit}>
+				<input type="text" name="option" />
+				<button>Add</button>
+			</form>
+		</div>
+	);
+	ReactDOM.render(template, appRoot);
+};
+
+renderApp();
